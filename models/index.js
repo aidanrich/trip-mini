756a0c0557820f24e2d2a4e767aacf835beab491
aorich <rich.aidan@gmail.com>
2021-10-14 13:06:26 -0400

    files added

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Trips = require('./Trips');
+const Traveller = require('./Traveller');
+const Location = require('./Location');
+
+module.exports = {
+    Trips,
+    Location,
+    Traveller,
+};
